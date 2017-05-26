1f26694c6bd6351c9533eb6369ad623d6769dce4
Rayek <rayek.elfin@gmail.com>
2017-05-26 09:56:53 -0700

    Code Optimization
    Optimized code found here:
    https://godotdevelopers.org/forum/discussion/18435/bunny-mark-performance

--- godot/bunny.gd
+++ godot/bunny.gd
@@ -1,43 +1,28 @@
-
 extends Sprite
-
-var vx = randi() % 200 + 50
-var vy = randi() % 200 + 50
-var ay = 980
+var v = Vector2(randi() % 200 + 50,randi() % 200 + 50 )
+const ay = 980
+var pos
 
 func _ready():
-	set_process(true)
-	
-	var pos = get_pos()
-	pos.x = 50
-	pos.y = 50
-	set_pos(pos)
-
+    set_pos(Vector2(50,50))
+    pos = get_pos()
+    set_process(true)
 
 func _process(delta):
-	var pos = get_pos()
-	
-	pos.x += vx * delta
-	pos.y += vy * delta
-	
-	vy += ay * delta
-	
-	if pos.x > 800:
-		vx = -vx
-		pos.x = 800
-		
-	elif pos.x < 0:
-		vx = abs(vx)
-		pos.x = 0
-		
-	elif pos.y > 600:
-		vy *= -0.85 
-		pos.y = 600
-		if randf() > 0.5:
-			vy = -(rand_range(50,1150))
-		
-	elif pos.y < 0:
-		vy = 0
-		pos.y = 0
-	
-	set_pos(pos)
+    v.y += ay * delta
+    pos += Vector2(v) * delta
+    if pos.y > 600:
+        v.y *= -0.85 
+        pos.y = 600
+        if randf() > 0.5:
+            v.y = -(rand_range(50,1150))
+    elif pos.y < 0:
+        v.y = 0
+        pos.y = 0   
+    elif pos.x > 800:
+        v.x = -v.x
+        pos.x = 800
+    elif pos.x < 0:
+        v.x = abs(v.x)
+        pos.x = 0
+    set_pos(pos)
