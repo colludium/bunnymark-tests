cbe93ab29bc51dde71fe95368441dda34959fecc
Rayek <rayek.elfin@gmail.com>
2017-05-26 09:59:04 -0700

    Main.gd Code Optimization
    Found here:
    https://godotdevelopers.org/forum/discussion/18435/bunny-mark-performance

--- godot/main.gd
+++ godot/main.gd
@@ -1,38 +1,38 @@
 extends Node2D
-
 var bunny = preload('res://bunny.scn')
 var bunnyCount = 0
 var elapsed = 0
+var NODE_fps
+var NODE_bunnyCount
+var fps
+var BunnySpawnIterator
+var BunnyInstance
 
 func _ready():
-	# Initalization here
-	set_process(true)
-	addBunny(10)
-	pass
-
-
+    # Initalization here
+    NODE_fps = get_node("fps")
+    NODE_bunnyCount = get_node("bunnyCount")
+    set_process(true)
+    addBunny(10)
 
 func _process(delta):
-	if Input.is_action_pressed('mouse_down'):
-		addBunny(10)
-		
-	elapsed = elapsed + delta
-	
-	# Update fps text once per second
-	if elapsed > 1:
-		var fps = OS.get_frames_per_second()
-		get_node("fps").set_text("FPS: " + str(fps))
-		
-		get_node("fps").set_as_toplevel(1)
-		get_node("bunnyCount").set_as_toplevel(1)
-		
-		elapsed = 0
+    if Input.is_action_pressed('mouse_down'):
+        addBunny(10)
+    elapsed = elapsed + delta
+    # Update fps text once per second
+    if elapsed > 1:
+        fps = OS.get_frames_per_second()
+        NODE_fps.set_text("FPS: " + str(fps))
+        NODE_fps.set_as_toplevel(1)
+        NODE_bunnyCount.set_as_toplevel(1)
+        elapsed = 0
 
 func addBunny(n):
-	for i in range(n):
-		var b = bunny.instance()
-		add_child(b)
-		bunnyCount = bunnyCount + 1
-	
-	var count = "Bunnies: " + str(bunnyCount)
-	get_node("bunnyCount").set_text(count)
+    BunnySpawnIterator = 0
+    while BunnySpawnIterator < n:
+        BunnySpawnIterator += 1
+        BunnyInstance= bunny.instance()
+        add_child(BunnyInstance)
+        bunnyCount = bunnyCount + 1
+    var count = "Bunnies: " + str(bunnyCount)
+    NODE_bunnyCount .set_text(count)
